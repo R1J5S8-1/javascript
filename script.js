63a1582dafec900c23735f842b87a28ae16fa7cb
R1J5S8-1 <rajeshrishanth0931@gmail.com>
2024-07-10 21:58:29 +0530

    comment

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // display numbers from 1 to 100
-
+console.log("Numbers from 1 to 100: ");
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
 
 // display odd numbers from 1 to 100
+console.log("Odd numbers from 1 to 100: ");
 for (let i = 1; i <= 100; i++) {
     if (i % 2 !== 0) {
         console.log(i);
@@ -12,6 +13,7 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // display even numbers from 1 to 100
+console.log("Even numbers from 1 to 100: ");
 for (let i = 1; i <= 100; i++) {
     if (i % 2 === 0) {
         console.log(i);
@@ -19,6 +21,7 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // display prime numbers from 1 to 100
+console.log("Prime numbers from 1 to 100: ");
 for (let i = 2; i <= 100; i++) {
     let isPrime = true;
     for (let j = 2; j < i; j++) {
